9342e83a7298a2362e1c132b7704cd795e97b52a
Amnaaftab123 <65035812+Amnaaftab123@users.noreply.github.com>
2020-07-08 04:11:13 +0500

    With WebGL

---
+++ Assets/Scripts/splash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        Invoke("Startnewscene", 2);
+    }
+
+    void Startnewscene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
